f0362f44657c9ad1ad506e8ad14c579344a9826b
Jason_Guo <76830748+Jason-Guo2008@users.noreply.github.com>
2021-01-08 22:10:00 +0800

    Add files via upload

---
+++ 完全背包问题.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int p[4]={0,1500,3000,2000};
+int w[4]={0,1,4,3};
+int dp[10][10]={0};
+int main(){
+	for(int i=1;i<4;i++){
+		for(int j=1;j<5;j++){
+			if(j>=w[i]){
+				dp[i][j]=max(p[i] + dp[i][j-w[i]],dp[i-1][j]);
+			}
+			else{
+				dp[i][j]=dp[i-1][j];//我有一个j容量的背包，如果装不下那个东西，那我这背包的最大价值就等于没有那个东西的时候 
+			}
+			cout<<dp[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
+
+ 
---
+++ 最长公共子序列（动态规划）.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[5000][5000] = {0};
+int main(){
+	string a;
+	string b;
+	cin >> a >> b;
+	for(int i=1;i<=a.size();i++){
+		for(int j=1;j<=b.size();j++){
+			if(a[i] == b[j]){
+				dp[i][j] = dp[i-1][j-1] + 1;
+			}
+			else{
+				dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+			}
+		}
+	}
+	cout << dp[a.size()][b.size()];
+	return 0;
+}
